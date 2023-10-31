e8acf4625d6916a83c9698a06195549346ec6c5d
Noah Galvez Montes <ngalvez-@c3r4s6.42malaga.com>
2023-10-31 11:29:05 +0100

    31oct

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngalvez- <ngalvez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 10:20:20 by ngalvez-          #+#    #+#             */
+/*   Updated: 2023/10/31 10:35:14 by ngalvez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+# endif
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngalvez- <ngalvez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 10:44:08 by ngalvez-          #+#    #+#             */
+/*   Updated: 2023/10/31 11:25:57 by ngalvez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+			return (&str[i]);
+		else
+			i++;
+	}
+	if (str[i] == (char)c)
+		return (&str[i]);
+	return (NULL);
+}
+
+
