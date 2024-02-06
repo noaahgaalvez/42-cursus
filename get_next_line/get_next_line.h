8feff199bc0a57dc183589f5d35611f9178043fb
Noah <ngalmon076@iesfuengirola.es>
2024-02-06 11:29:38 +0100

    Comienzo del bonus

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -19,9 +19,10 @@
 int		ft_strlen(char *str);
 char	*ft_strchr(char *str, int c);
 char	*ft_strjoin(char *s1, char *s2);
-char	*get_next_line(int fd);
+
 char	*ft_line(char *str);
 char	*ft_save(char *str);
 char	*ft_read(int fd, char *str);
+char	*get_next_line(int fd);
 
 #endif
